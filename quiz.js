d0bb5bc18633f2537419a0dcfab9c7880d7781bb
Winglet80 <frederik@sonsky.se>
2021-08-12 13:58:56 +0200

    Added multiple choice

--- quiz.js
+++ quiz.js
@@ -1,21 +1,35 @@
 let questions = [
-    {question: "Earth is flat.", answer: false},
-    {question: "Java is a programming language", answer: true}
+    {question: "Earth is flat.", options: ["True", "False"], answer: 1},
+    {question: "Earth is flat.", options: ["True", "False"], answer: 1},
+    {question: "Java is a programming language", options: ["True", "False"], answer: 0}
 ]
 
-let count = 0
+let count = -1
 let points = 0
-document.getElementById("question").innerText = questions[count].question
+generateNewQuestion()
 
-function answerCheck(answer){
+function answerCheck(){
     if(count != questions.length-1){
         document.getElementById("NextQuestionButton").classList.remove("hide")
     }
     else{
         document.getElementById("ShowScoreButton").classList.remove("hide")
     }
-    document.getElementById("TrueButton").classList.add("hide")
-    document.getElementById("FalseButton").classList.add("hide")
+    // document.getElementById("TrueButton").classList.add("hide")
+    // document.getElementById("FalseButton").classList.add("hide")
+    document.getElementById("answerButton").classList.add("hide")
+
+    let options = document.getElementsByName("answer")
+    let answer = -1
+    console.log("Options", options)
+    for(let option of options) {
+        if (option.checked) {
+            answer = parseInt(option.value)
+            break
+        }
+    }
+    console.log("Answer", answer)
+
     if(questions[count].answer === answer){
         document.getElementById("alert").innerHTML = `<div class="alert alert-success" role="alert">
         Correct!
@@ -35,10 +49,25 @@ function generateNewQuestion(){
     count++
     document.getElementById("question").innerText = questions[count].question
     document.getElementById("NextQuestionButton").classList.add("hide")
-    document.getElementById("TrueButton").classList.remove("hide")
-    document.getElementById("FalseButton").classList.remove("hide")
-    document.getElementById("alert").innerHTML =""
+    document.getElementById("answerButton").classList.remove("hide")
+    //document.getElementById("TrueButton").classList.remove("hide")
+    //document.getElementById("FalseButton").classList.remove("hide")
+    document.getElementById("alert").innerHTML = ""
     
+    let html = ""
+    let pos = 0
+    for (let option of questions[count].options) {
+        html += `
+        <div class="form-check" style="text-align: left">
+            <input class="form-check-input" type="radio" name="answer" value="${pos}">
+            <label class="form-check-label" for="exampleRadios1">
+                ${option}
+            </label>
+        </div>`
+        pos++
+    }
+
+    document.getElementById("options").innerHTML = html
 }
 
 function showScore(){
